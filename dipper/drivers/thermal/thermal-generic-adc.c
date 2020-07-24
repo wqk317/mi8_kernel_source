e3482d1a7ea6021de2fc8f3178496b4b043bb727
吾爱317 <wqk317226700@gmail.com>
2020-07-24 10:35:48 +0800

    repo init

---
+++ dipper/drivers/thermal/thermal-generic-adc.c
@@ -0,0 +1,18 @@
+/*
+ * Generic ADC thermal driver
+ *
+ * Copyright (C) 2016 NVIDIA CORPORATION. All rights reserved.
+ *
+ * Author: Laxman Dewangan <ldewangan@nvidia.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <linux/iio/consumer.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/slab.h>
+#include <linux/thermal.h>
+
